2af73259b4af062e94316386da1f4a62cc842001
Anmol Ratan Tiwari <anmolratantiwari@coer.ac.in>
2025-04-12 19:58:17 +0530

    Demo

--- Project/app.js
+++ Project/app.js
@@ -1,8 +1,64 @@
+import React from "react";
+import ReactDOM from "react-dom/client"; 
 
-const heading  = React.createElement("h1",{id:"heading"}, "Hello World from React");
+const Header = () => {
+    return (
+        <div className="header">
+            <img id="logo"src ="https://img.freepik.com/premium-vector/online-food-app-icon-food-shop-location-logo-also-online-resturent-location-template_608547-155.jpg" alt="log" />
+           <div className ="nav">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+            </div>
+        </div>
+    )
+};
+const RestorentCard = () => {
+return (
+    <div className="restorent-card">
+    <img className="res-logo"
+    src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRETxQtMecTxrMgY5eFO9Rij1unZtEi8voL0Q&s"
+    />
+    <h5>Om Dhaba</h5>
+    <h5>Rating 5/5</h5>
+    <h5> Dal Chawal</h5>
+    </div>
+)
+};
+const Body = () => {
+return (
+    <div className="body">
+        <div className="search">
+        <input type="text" placeholder="Search for food" />
+        <button>Search</button>
+        </div>
+        <div className ="res-container">
+        <RestorentCard />
+        <RestorentCard />
+        <RestorentCard />
+        <RestorentCard />
+        <RestorentCard />
+        </div>
+        <h1>Food Delivery App</h1>
+        <p>Order your favorite food online and get it delivered to your doorstep.</p>
+        <button>Order Now</button>
+    </div>
+)
+};
+const Footer = ()=>{
+    return (<div className="Footer"> This is a footer</div>)
+};
+const App = () => {
+return (
+    <div className="app">
+        <Header />
+        <Body />
+        <Footer />
+    </div>
+)
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(heading);
-console.log(root);
-
-root.render(heading);
+root.render(<App />); // Render the React element to the DOM    
